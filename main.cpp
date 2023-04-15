7ba35bdcd64de5cf12b239fa4b1b35c5150b58bc
R. ALOUI <aloui.radhouane@gmail.com>
2023-04-15 11:04:30 +0100

    Constructeur par defaut et fnction print

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
-
-// using namespace std; Ne pas utiliser car elle charge tout l'espace de nom dans la mémoire.
 using std::cout;
+// using namespace std; Ne pas utiliser car elle charge tout l'espace de nom dans la mémoire.
+// Création d'un nouveau type Quotient
+class Quotient{
+public:
+  // Constructeur par défaut
+  Quotient():num(0), den(1){}
+  /*Quotient(){
+    num = 0;
+    den = 1;
+  }*/
+  // Afficher le quotient 
+  void print(){
+    cout << num << "/" << den;
+  }
+private:
+  int num;
+  int den;
+};
 
 int main (){
-  cout << "Bonjour MT";
+  Quotient q;
+
+  q.print();
   return 0;
 }
