e4420086281cf600063970d976fe299aa279ead9
R. ALOUI <aloui.radhouane@gmail.com>
2023-04-15 10:33:10 +0100

    Programme bonjour MT

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// using namespace std; Ne pas utiliser car elle charge tout l'espace de nom dans la mémoire.
+using std::cout;
+
+int main (){
+  cout << "Bonjour MT";
+  return 0;
+}
