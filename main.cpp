a0de45f330255434a096921cc6e8f6bfeba6b671
R. ALOUI <aloui.radhouane@gmail.com>
2023-04-15 11:26:12 +0100

    Constructeur parametre

--- main.cpp
+++ main.cpp
@@ -5,11 +5,20 @@ using std::cout;
 class Quotient{
 public:
   // Constructeur par défaut
-  Quotient():num(0), den(1){}
+  Quotient():num(0), den(1){
+    cout << "Constructeur par defaut\n";
+  }
   /*Quotient(){
     num = 0;
     den = 1;
   }*/
+  // Constructeur paramétré
+  Quotient(int pnum, int pden):num(pnum), den(pden){
+    if(den == 0){
+      den = 1;
+    }
+    cout << "Constructeur parametre\n";
+  }
   // Afficher le quotient 
   void print(){
     cout << num << "/" << den;
@@ -21,7 +30,9 @@ private:
 
 int main (){
   Quotient q;
+  Quotient q1(1, 0);
 
-  q.print();
+  q.print(); cout << "\n";
+  q1.print();
   return 0;
 }
