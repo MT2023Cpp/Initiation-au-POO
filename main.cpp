049c674384e93ea1e2194428ff1959b3094ed8d5
R. ALOUI <aloui.radhouane@gmail.com>
2023-04-15 11:45:26 +0100

    Separation entre la declaration et l'implementation de la classe

--- main.cpp
+++ main.cpp
@@ -1,32 +1,36 @@
 #include <iostream>
 using std::cout;
 // using namespace std; Ne pas utiliser car elle charge tout l'espace de nom dans la mémoire.
-// Création d'un nouveau type Quotient
+// Création d'un nouveau type Quotient (Déclaration / interface)
 class Quotient{
 public:
-  // Constructeur par défaut
-  Quotient():num(0), den(1){
-    cout << "Constructeur par defaut\n";
-  }
-  /*Quotient(){
-    num = 0;
-    den = 1;
-  }*/
-  // Constructeur paramétré
-  Quotient(int pnum, int pden):num(pnum), den(pden){
-    if(den == 0){
-      den = 1;
-    }
-    cout << "Constructeur parametre\n";
-  }
-  // Afficher le quotient 
-  void print(){
-    cout << num << "/" << den;
-  }
+  Quotient(); // Constructeur par défaut
+  Quotient(int pnum, int pden); // Constructeur paramétré
+  void print(); // Afficher le quotient 
 private:
   int num;
   int den;
 };
+
+// L'implémentation
+Quotient::Quotient():num(0), den(1){
+  cout << "Constructeur par defaut\n";
+}
+  /*Quotient(){
+    num = 0;
+    den = 1;
+  }*/
+  
+Quotient::Quotient(int pnum, int pden):num(pnum), den(pden){
+  if(den == 0){
+    den = 1;
+  }
+  cout << "Constructeur parametre\n";
+}
+  
+void Quotient::print(){
+  cout << num << "/" << den;
+}
 
 int main (){
   Quotient q;
